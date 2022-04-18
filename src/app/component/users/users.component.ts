610e72619c68e38c89e35b3bbaee46c83958ac30
vanta blanta <vantablanta@gmail.com>
2022-04-19 01:40:58 +0300

    color chnage on user c

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/service/users.service';
 import { UserClass } from 'src/app/user-class';
 
-import { faSearch, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faUsers, faFileCode, faHeart,faEye } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-users',
@@ -13,6 +13,9 @@ import { faSearch, faUsers } from '@fortawesome/free-solid-svg-icons';
 export class UsersComponent implements OnInit {
   faSearch = faSearch
   faUsers = faUsers
+  faFileCode = faFileCode
+  faHeart = faHeart
+  faEye = faEye
 
   users! : UserClass
 
@@ -26,8 +29,13 @@ export class UsersComponent implements OnInit {
     this.usersService.filterUser(this.userName).subscribe(
       (data: any) => {
         this.users = data
+        console.log(this.users)
       }
     )
+  }
+
+  findRepos(){
+    
   }
 
   ngOnInit(): void {
